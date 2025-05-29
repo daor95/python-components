6b34ff49dcb1012eae632e4fb40fa8c63ec5fa62
David Asensio Ortiz de Pinedo <davidasensio95@gmail.com>
2025-05-29 17:01:45 +0200

    PIOT-CDA-09-005

--- src/main/python/programmingtheiot/cda/connection/CoapClientConnector.py
+++ src/main/python/programmingtheiot/cda/connection/CoapClientConnector.py
@@ -83,9 +83,27 @@ class CoapClientConnector(IRequestResponseClient):
 			timeout=timeout
 		)
 
-	def sendDeleteRequest(self, resource: ResourceNameEnum = None, name: str = None, enableCON: bool = False, timeout: int = IRequestResponseClient.DEFAULT_TIMEOUT) -> bool:
-		logging.info("sendDeleteRequest called.")
-		return False
+	def sendDeleteRequest(
+			self,
+			resource: ResourceNameEnum = None,
+			name: str = None,
+			enableCON: bool = False,
+			timeout: int = IRequestResponseClient.DEFAULT_TIMEOUT
+	) -> bool:
+		if resource or name:
+			resourcePath = self._createResourcePath(resource, name)
+
+			logging.info("Issuing Async DELETE to path: " + resourcePath)
+
+			asyncio.get_event_loop().run_until_complete(
+				self._handleDeleteRequest(
+					resourcePath=resourcePath,
+					enableCON=enableCON
+				)
+			)
+		else:
+			logging.warning("Can't issue Async DELETE - no path or path list provided.")
+
 
 	def sendGetRequest(
 			self,
@@ -318,6 +336,32 @@ class CoapClientConnector(IRequestResponseClient):
 			return
 
 		logging.info('POST response received: %s', response.payload)
+
+	async def _handleDeleteRequest(self, resourcePath: str = None, enableCON: bool = False):
+		try:
+			msgType = NON
+
+			if enableCON:
+				msgType = CON
+
+			full_uri = f"coap://{self.host}:{self.port}/{resourcePath.lstrip('/')}"
+			msg = Message(mtype=msgType, code=Code.DELETE, uri=full_uri)
+			req = self.coapClient.request(msg)
+			responseData = await req.response
+
+			self._onDeleteResponse(responseData)
+
+		except Exception as e:
+			# TODO: for debugging, you may want to optionally include the stack trace, as shown
+			logging.warning("Failed to process DELETE request for path: " + resourcePath)
+			traceback.print_exception(type(e), e, e.__traceback__)
+
+	def _onDeleteResponse(self, response):
+		if not response:
+			logging.warning('DELETE response invalid. Ignoring.')
+			return
+
+		logging.info('DELETE response received: %s', response.payload)
 
 
 
